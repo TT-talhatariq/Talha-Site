0642575ddab27ad1b8e09aa7922790b9ad99e623
Talha Tariq <talhacoder01@gmail.com>
2025-11-20 17:34:43 +0500

    Done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,14 +28,21 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1028001395588218&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=792263316561861&ev=PageView&noscript=1"
+            alt=""
+          />
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1507662913842457&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
-        {/* Meta Pixel Code */}
-        <Script id="meta-pixel" strategy="afterInteractive">
+        {/* Meta Pixel Code - First Pixel */}
+        <Script id="meta-pixel-1" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -45,7 +52,22 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1028001395588218');
+            fbq('init', '792263316561861');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* Meta Pixel Code - Second Pixel */}
+        <Script id="meta-pixel-2" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1507662913842457');
             fbq('track', 'PageView');
           `}
         </Script>
