1fb399b4f03eae3bb3350f47e1cb7c983b9d246c
Talha Tariq <talhacoder01@gmail.com>
2026-03-06 15:57:12 +0500

    app fix

--- src/pages/WebDev/checkout.tsx
+++ src/pages/WebDev/checkout.tsx
@@ -61,7 +61,7 @@ export default function CheckoutWebDev() {
         
         // Validate payment screenshot
         const validPaymentKeywords = [
-        'Talha Tariq', '03257367496', 'talha tariq'
+        'Talha Tariq', '03257460090', 'talha tariq'
         ];
         
         const hasValidKeywords = validPaymentKeywords.some(keyword => 
